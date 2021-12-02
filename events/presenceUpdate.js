c933f6a40b4ad81e3c3631f8b8935e66969d7fd2
grassehh <sylvain.becuwe@gmail.com>
2021-12-02 20:52:06 +0100

    Added promise in presenceUpdate

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,6 +1,5 @@
 const teamService = require('../services/teamService');
 const matchmakingService = require('../services/matchmakingService');
-const interactionUtils = require('../services/interactionUtils');
 
 module.exports = {
 	name: 'presenceUpdate',
@@ -11,15 +10,14 @@ module.exports = {
 			if (teams.length > 0) {
 				let updateResult = await teamService.removeUserFromTeams(userId)
 				if (updateResult.modifiedCount > 0) {
-					await matchmakingService.removeUserFromAllChallenges(userId)
-					await teamService.removeUserFromAllLineupQueue(userId)
-
-					let allChannelIds = teams.flatMap(team => team.lineups).filter(lineup => lineup.roles.some(role => role.user?.id === userId)).map(lineup => lineup.channelId)
-					for (let channelId of allChannelIds) {
-						newPresence.client.channels.fetch(channelId).then((channel) => {
-							channel.send(`⚠ Player ${newPresence.user} went offline or is afk. He has been removed from the lineup`)
-						})
-					}
+					Promise.all([matchmakingService.removeUserFromAllChallenges(userId), teamService.removeUserFromAllLineupQueue(userId)]).then(res => {
+						let allChannelIds = teams.flatMap(team => team.lineups).filter(lineup => lineup.roles.some(role => role.user?.id === userId)).map(lineup => lineup.channelId)
+						for (let channelId of allChannelIds) {
+							newPresence.client.channels.fetch(channelId).then((channel) => {
+								channel.send(`⚠ Player ${newPresence.user} went offline or is afk. He has been removed from the lineup`)
+							})
+						}
+					})
 				}
 			}
 		}
