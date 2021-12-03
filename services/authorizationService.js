93e596e7245f78e96f282985d84943078e6b30da
grassehh <sylvain.becuwe@gmail.com>
2021-12-03 19:55:59 +0100

    Command permission role check now ignores the case

--- services/authorizationService.js
+++ services/authorizationService.js
@@ -5,5 +5,5 @@ exports.BOT_ADMIN_ROLE = 'PSO MM ADMIN'
 exports.isAllowedToExecuteCommand = (command, member) => {
     return !command.authorizedRoles
         || member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
-        || member.roles.cache.some(role => command.authorizedRoles.includes(role.name) === true)
+        || member.roles.cache.some(role => command.authorizedRoles.includes(role.name.toUpperCase()) === true)
 }
