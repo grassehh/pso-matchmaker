5f64f2356735dce580a212734e4794bff1f2dca6
grassehh <sylvain.becuwe@gmail.com>
2021-12-08 10:19:40 +0100

    Added auto role upgrade

--- services/statsService.js
+++ services/statsService.js
@@ -1,6 +1,61 @@
+const { PSO_EU_MINIMUM_LINEUP_SIZE_LEVELING } = require("../constants")
 const { Stats } = require("../mongoSchema")
+const { handle } = require("../utils")
 
 exports.DEFAULT_LEADERBOARD_PAGE_SIZE = 10
+
+function getLevelingRoleIdsFromStats(userStats) {
+    let roles = []
+    if (userStats.numberOfGames > 50) {
+        roles.push(process.env.PSO_EU_DISCORD_CONFIRMED_ROLE_ID)
+    }
+    if (userStats.numberOfGames > 250) {
+        roles.push(process.env.PSO_EU_DISCORD_ADVANCED_ROLE_ID)
+    }
+    if (userStats.numberOfGames > 800) {
+        roles.push(process.env.PSO_EU_DISCORD_VETERAN_ROLE_ID)
+    }
+    return roles
+}
+
+async function findElligibleStatsForLevelling(userIds) {
+    return await Stats.aggregate([
+        {
+            $match: {
+                userId: {
+                    $in: userIds
+                },
+                lineupSize: {
+                    $gte: PSO_EU_MINIMUM_LINEUP_SIZE_LEVELING
+                }
+            }
+        },
+        {
+            $group: {
+                _id: '$userId',
+                numberOfGames: {
+                    $sum: '$numberOfGames',
+                }
+            }
+        }
+    ])
+}
+
+exports.upgradePlayersLevel = async (interaction, userIds) => {
+    const allElligibleStats = await findElligibleStatsForLevelling(userIds)
+
+    return Promise.all(allElligibleStats.map(async elligibleStats => {
+        const [psoEuGuild] = await handle(interaction.client.guilds.fetch(process.env.PSO_EU_DISCORD_GUILD_ID))
+        if (!psoEuGuild) {
+            return
+        }
+        const levelingRoleIds = getLevelingRoleIdsFromStats(elligibleStats)
+        const [member] = await handle(psoEuGuild.members.fetch(elligibleStats._id))
+        if (member) {
+            handle(member.roles.add(levelingRoleIds))
+        }
+    }))
+}
 
 exports.incrementGamesPlayed = async (guildId, lineupSize, users) => {
     let bulks = users.map((user) => ({
@@ -30,7 +85,7 @@ exports.countNumberOfPlayers = async (guildId, lineupSizes = []) => {
     }
     if (lineupSizes.length > 0) {
         match.lineupSize = { $in: lineupSizes.map(size => parseInt(size)) }
-    }    
+    }
     return (await Stats.distinct('userId', match)).length
 }
 
