b07f93bda65a881266812ae881d41497f41fa832
grassehh <sylvain.becuwe@gmail.com>
2022-01-02 17:24:06 +0100

    Added leave all command

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const dotenv = require('dotenv');
-const { Permissions } = require('discord.js');
 
 dotenv.config()
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(`../commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
