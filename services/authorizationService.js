dbe265a67c5b29e1e57b9617f3712ab26bdb5a06
grassehh <sylvain.becuwe@gmail.com>
2022-02-19 21:13:16 +0100

    Clear role button (for mix) is now only for MM admins

--- services/authorizationService.js
+++ services/authorizationService.js
@@ -7,8 +7,13 @@ exports.isBotAllowed = (interaction) => {
         && interaction.channel.permissionsFor(interaction.guild.me).has([Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES])
 }
 
-exports.isAllowedToExecuteCommand = async (command, member) => {
+exports.isAllowedToExecuteCommand = (command, member) => {
     return !command.authorizedRoles
         || member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
         || member.roles.cache.some(role => command.authorizedRoles.includes(role.name.toUpperCase()) === true)
+}
+
+exports.isMatchmakingAdmin = (member) => {
+    return member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
+        || member.roles.cache.some(role => role.name.toUpperCase() === this.BOT_ADMIN_ROLE)
 }
