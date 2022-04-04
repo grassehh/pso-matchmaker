a1f03dba378d14709c30bacab14fd64f9fddb76b
grassehh <sylvain.becuwe@gmail.com>
2022-04-04 22:35:08 +0200

    Added ban/unban user from id

--- interactions/commands/ban.js
+++ interactions/commands/ban.js
@@ -9,9 +9,9 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
         .setDescription('Ban a player from using the bot in this team')
-        .addStringOption(option => option.setName('player_mention')
+        .addStringOption(option => option.setName('player')
             .setRequired(true)
-            .setDescription('The mention of the player to ban. For example: @Player'))
+            .setDescription('The mention (@...) or the id of the player to ban. For example: @Player or 123456789012345678'))
         .addIntegerOption(option => option.setName('duration')
             .setRequired(false)
             .setDescription('The duration of the ban in days. A value of -1 means unlimited ban. (Default value is 1)')),
@@ -29,36 +29,38 @@ module.exports = {
             return
         }
 
-        const playerMention = interaction.options.getString('player_mention')
-        const userId = getUserIdFromMention(playerMention)
-        if (userId) {            
-            if (userId === interaction.client.user.id) {
-                await interaction.reply({ content: `⛔ You cannot ban the bot !`, ephemeral: true })
-                return
-            }
-            if (userId === interaction.user.id) {
-                await interaction.reply({ content: `⛔ You surely don't want to ban yourself !`, ephemeral: true })
-                return
-            }
+        const player = interaction.options.getString('player')
+        let userId = player.includes('@') ? getUserIdFromMention(player) : player
 
-            const now = Date.now()
-            const duration = interaction.options.getInteger('duration')
-            let expireAt = null
-            if (duration > 0) {
-                expireAt = now + duration * 24 * 60 * 60 * 1000
-            } else if (duration != -1) {
-                expireAt = now + 24 * 60 * 60 * 1000
-            }
-            await Bans.updateOne({ userId, guildId: team.guildId }, { userId, expireAt }, { upsert: true })
-
-            let formattedDate
-            if (expireAt) {
-                formattedDate = new Date(expireAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit', hour: '2-digit', minute: 'numeric' })
-            }
-            await interaction.reply({ content: `Player ${playerMention} is now ${formattedDate ? `banned until ${formattedDate}` : 'permanently banned'}`, ephemeral: true })
+        const user = await interaction.client.users.resolve(userId)
+        if (!user) {
+            await interaction.reply({ content: `⛔ User '${player}' not found`, ephemeral: true })
             return
         }
 
-        await interaction.reply({ content: `⛔ User '${playerMention}' not found`, ephemeral: true })
+        if (user.id === interaction.client.user.id) {
+            await interaction.reply({ content: `⛔ You cannot ban the bot !`, ephemeral: true })
+            return
+        }
+        if (user.id === interaction.user.id) {
+            await interaction.reply({ content: `⛔ You surely don't want to ban yourself !`, ephemeral: true })
+            return
+        }
+
+        const now = Date.now()
+        let expireAt = null
+        if (duration > 0) {
+            expireAt = now + duration * 24 * 60 * 60 * 1000
+        } else if (duration != -1) {
+            expireAt = now + 24 * 60 * 60 * 1000
+        }
+        await Bans.updateOne({ userId: user.id, guildId: team.guildId }, { userId: user.id, expireAt }, { upsert: true })
+
+        let formattedDate
+        if (expireAt) {
+            formattedDate = new Date(expireAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit', hour: '2-digit', minute: 'numeric' })
+        }
+        await interaction.reply({ content: `Player **${user.username}** is now ${formattedDate ? `banned until ${formattedDate}` : 'permanently banned'}`, ephemeral: true })
+        return
     }
 }
