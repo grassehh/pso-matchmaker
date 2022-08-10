f249cc20141bbae071114da895f621feb591dba4
grassehh <sylvain.becuwe@gmail.com>
2022-08-10 12:30:23 +0200

    feat: Added default permission for ban commands

--- bot/src/interactions/commands/unban.ts
+++ bot/src/interactions/commands/unban.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { ChatInputCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, PermissionFlagsBits } from "discord.js";
 import { BOT_ADMIN_ROLE } from "../../constants";
 import { ICommandHandler } from "../../handlers/commandHandler";
 import { interactionUtils } from "../../services/interactionUtils";
@@ -11,7 +11,8 @@ export default {
         .setDescription('Unban a player from using the bot in this team')
         .addUserOption(option => option.setName('player')
             .setRequired(true)
-            .setDescription('The mention (@...) or the id of the player to ban. For example: @Player or 123456789012345678')),
+            .setDescription('The mention (@...) or the id of the player to ban. For example: @Player or 123456789012345678'))
+            .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
     authorizedRoles: [BOT_ADMIN_ROLE],
     async execute(interaction: ChatInputCommandInteraction) {
         const team = await teamService.findTeamByGuildId(interaction.guildId!)
