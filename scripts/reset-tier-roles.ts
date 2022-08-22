e261a8696e12f34f144f7e4d9f7735839087e4e2
grassehh <sylvain.becuwe@gmail.com>
2022-08-22 15:54:13 +0200

    fix: /player_stats_downgrade now filters on region

---
+++ scripts/reset-tier-roles.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import { DEFAULT_RATING } from '../bot/src/constants';
+import { Stats, Team } from '../bot/src/mongoSchema';
+import dotenv = require('dotenv');
+dotenv.config()
+
+async function resetStats(): Promise<void> {
+    await mongoose.connect(process.env.MONGO_URI || '', { keepAlive: true })
+
+    await Stats.updateMany({}, {
+        $set: {
+            numberOfRankedWins: 0,
+            numberOfRankedDraws: 0,
+            numberOfRankedLosses: 0,
+            attackRating: DEFAULT_RATING,
+            midfieldRating: DEFAULT_RATING,
+            defenseRating: DEFAULT_RATING,
+            goalKeeperRating: DEFAULT_RATING,
+            mixCaptainsRating: DEFAULT_RATING
+        }
+    })
+    await Team.updateMany({}, {
+        $set: {
+            rating: DEFAULT_RATING
+        }
+    })
+}
+
+resetStats().finally(async () => {
+    console.log("Reset stats finished");
+    await mongoose.disconnect();
+    process.exit();
+})
+
