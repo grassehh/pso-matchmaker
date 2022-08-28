39263a3969d07fa32443b5f198506eaeb3f0b94f
grassehh <sylvain.becuwe@gmail.com>
2022-08-28 21:29:53 +0200

    feat: Added end season script

--- scripts/end-season.ts
+++ scripts/end-season.ts
@@ -1,21 +1,49 @@
 import mongoose from 'mongoose';
-import { Stats } from '../bot/src/mongoSchema';
+import { DEFAULT_RATING } from '../bot/src/constants';
+import { Stats, Team } from '../bot/src/mongoSchema';
+import { Region, regionService } from '../bot/src/services/regionService';
 import dotenv = require('dotenv');
 dotenv.config()
 
-async function resetStats(): Promise<void> {
+async function endSeason(): Promise<void> {
     await mongoose.connect(process.env.MONGO_URI || '', { keepAlive: true })
-    await Stats.updateMany({}, {
-        $set: {
-            numberOfRankedWins: 0,
-            numberOfRankedDraws: 0,
-            numberOfRankedLosses: 0
+    await Team.updateMany({}, { rating: DEFAULT_RATING })
+    const regionData = regionService.getRegionData(Region.EUROPE)
+    const newTier1PlayersRating = regionData.tier2Threshold! - 50
+    const newTier2PlayersRating = (regionData.tier2Threshold! + regionData.tier3Threshold!) / 2
+    const newTier3PlayersRating = regionData.tier3Threshold! + 50
+    const regionStats = await Stats.find({ region: regionData.region })
+    const bulks: any = []
+    for (const stats of regionStats) {
+        const tierRoleId = regionService.getTierRoleId(regionData.region, stats.rating)
+        let rating = DEFAULT_RATING
+        if (tierRoleId === regionData.tier1RoleId) {
+            rating = newTier1PlayersRating
+        } else if (tierRoleId === regionData.tier2RoleId) {
+            rating = newTier2PlayersRating
+        } else if (tierRoleId) {
+            rating = newTier3PlayersRating
         }
-    })
+        bulks.push({
+            updateOne: {
+                filter: { _id: stats._id },
+                update: {
+                    $set: {
+                        rating,
+                        numberOfRankedWins: 0,
+                        numberOfRankedDraws: 0,
+                        numberOfRankedLosses: 0
+                    }
+                }
+            }
+        })
+    }
+
+    await Stats.bulkWrite(bulks)
 }
 
-resetStats().finally(async () => {
-    console.log("Ended season succesfully");
+endSeason().finally(async () => {
+    console.log("Season ended");
     await mongoose.disconnect();
     process.exit();
 })
