1ab1a72294ebbe5b4ed7506b81014d7c598b7880
grassehh <sylvain.becuwe@gmail.com>
2022-12-20 22:17:32 +0100

    feat: Improved leave-unused-servers script to check if the bot joined more than 1 week ago

--- scripts/leave-unused-servers.ts
+++ scripts/leave-unused-servers.ts
@@ -15,10 +15,10 @@ async function leaveUnusedServers() {
   await mongoose.connect(process.env.MONGO_URI || '', { keepAlive: true })
   const guilds = client.guilds.cache.values()
   for (let guild of guilds) {
-    console.log(`Checking guild with id ${guild.id}`)
     const team = await Team.findOne({ guildId: guild.id })
-    if (!team) {
-      console.log(`guild with id ${guild.id} has no team registered: leaving`)
+    const joinedAt = guild.members.me?.joinedAt
+    if (!team && joinedAt && (Math.abs(joinedAt.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 7)) {
+      console.log(`Guild ${guild.id} (${guild.name}) joined at ${joinedAt.toISOString()} has no team registered: leaving`)
       await guild.leave()
     }
   }
