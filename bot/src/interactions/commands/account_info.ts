5e78c36b5465cff995877b17cfe66c83425110d8
FlyingTurkman <99084104+FlyingTurkman@users.noreply.github.com>
2023-01-03 15:28:08 +0300

    Sharkman
    Comment removed

--- bot/src/interactions/commands/account_info.ts
+++ bot/src/interactions/commands/account_info.ts
@@ -51,8 +51,7 @@ export default {
                 { name: 'Steam ID', value: userInDb.steamId ? userInDb.steamId : '*Account not linked*', inline: true },
                 { name: 'Steam profile', value: userInDb.steamId ? `https://steamcommunity.com/profiles/${userInDb.steamId}` : '*Account not linked*', inline: true },
                 { name: '\u200B', value: '\u200B' },
-                //its for checking their old steam names etc.
-                { name: 'Steam details',value:userInDb.steamId?`https://steamid.uk/profile/${userInDb.steamId}`:'*Account not linked*',inline:true}
+                { name: 'Steam details', value:userInDb.steamId ? `https://steamid.uk/profile/${userInDb.steamId}` : '*Account not linked*', inline:true}
             ])
 
 
