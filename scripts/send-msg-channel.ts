2956baeb5eb899351c5b3f8ca3bda53eea167664
grassehh <sylvain.becuwe@gmail.com>
2023-02-06 23:17:30 +0100

    feat: Updated /help command

--- scripts/send-msg-channel.ts
+++ scripts/send-msg-channel.ts
@@ -21,56 +21,64 @@ async function sendMessage() {
 
     const setupEmbed = new EmbedBuilder()
         .setColor('#566573')
-        .setTitle(`How to configure the bot ?`)
+        .setTitle(`How to create your own team ?`)
         .setTimestamp()
-        .addFields([
-            { name: '1) Invite the bot on your discord', value: 'Click on the following link to invite the bot on your discord server: https://discord.com/api/oauth2/authorize?client_id=914818953707151420&permissions=2416077824&scope=bot%20applications.commands' },
-            { name: '2) Create your team', value: 'Use the **/team_create** command anywhere on your discord server to create your team. Make sure you select the correct region.' },
-            {
-                name: '3) Create lineups',
-                value: `
-                            On any channel in you discord server, use one of the following command to configure a lineup and get ready to play and face other teams
-                            **/lineup_create**: If you want to challenge other teams and mix
-                            **/lineup_create_mix**: If you want to play with your community only (not facing any team or mix)
-                            **/lineup_create_solo_queue**: Similar to **/lineup_create_mix**, but more anonymous. Players can't see who is signed in which position, and when the match starts, teams are shuffled based on each player rating
-                            **/lineup_create_captains**: If you want a Pick Up Game (PUG) with snake draft
-                            `
-            },
-            {
-                name: '4) Command Permissions',
-                value: `
-                            The following commands require users to either be the server administrator or to have a role name '**PSO MM ADMIN**'
-                            - **/team_create**
-                            - **/team_manage**
-                            - **/team_region**
-                            - **/team_delete**
-                            - **/teams**
-                            - **/lineup_create**
-                            - **/lineup_create_mix**
-                            - **/lineup_create_solo_queue**
-                            - **/lineup_create_captains**
-                            - **/lineup_delete**
-                            - **/match_manage**
-                            - **/ban**
-                            - **/unban**
-                            - **/bans**
-                            - **/account_info**
-                            `
-            },
-            {
-                name: '5) Official regional command permissions',
-                value: `
-                            The following commands can only be used on official regional discords. 
-                            - **/team_manage** with the *team_id* optional parameter
-                            - **/teams**
-                            - **/lineup_create_mix** with the *ranked* optional paramter set to **true**
-                            - **/lineup_create_solo_queue** with the *ranked* optional paramter set to **true**
-                            - **/lineup_create_captains** with the *ranked* optional paramter set to **true**
-                            - **/match_manage**
-                            - **/account_info**
-                            `
-            }
-        ])
+        .setDescription(`
+            **1) Invite the bot on your discord**
+            Click here [here](https://discord.com/api/oauth2/authorize?client_id=914818953707151420&permissions=2550295552&scope=applications.commands%20bot) to invite the bot on your discord server
+
+            2) Create your team
+            Use the **/team_create** command anywhere on your discord server to create your team. Make sure you select the correct region.
+
+            3) Create lineups                
+            On any channel in you discord server, use one of the following command to configure a lineup and get ready to play and face other teams
+            **/lineup_create**: If you want to challenge other teams and mix
+            **/lineup_create_mix**: If you want to play with your community only (not facing any team or mix)
+            **/lineup_create_solo_queue**: Similar to **/lineup_create_mix**, but more anonymous. Players can't see who is signed in which position, and when the match starts, teams are shuffled based on each player rating
+            **/lineup_create_captains**: If you want a Pick Up Game (PUG) with snake draft
+
+            4) Manage your team
+            Use the **/team_manage** command to edit your team at any time.
+            You can edit the captains/players list, team logo, team type and more.
+            Send recruitment offers for your team to the official Discord's mercato channel with the **/team_offer** command !
+
+            **5) Command Permissions**
+            The following commands require users to either be the server administrator or to have a role name '**PSO MM ADMIN**'
+            - **/team_create**
+            - **/team_manage**
+            - **/team_region**
+            - **/team_delete**
+            - **/team_ban**
+            - **/team_unban**
+            - **/team_bans**
+            - **/teams**                            
+            - **/lineup_clear**
+            - **/lineup_create**
+            - **/lineup_create_mix**
+            - **/lineup_create_solo_queue**
+            - **/lineup_create_captains**
+            - **/lineup_delete**
+            - **/lineup_name**
+            - **/match_manage**
+            - **/player_ban**
+            - **/player_unban**
+            - **/player_bans**
+            - **/player_stats_downgrade**
+            - **/account_info**
+
+            **6) Official Discords command permissions**
+            The following commands can only be used on official regional discords. 
+            - **/team_manage** with the *team_id* optional parameter
+            - **/team_ban**
+            - **/team_unban**
+            - **/teams**
+            - **/lineup_create_mix** with the *ranked* optional paramter set to **true**
+            - **/lineup_create_solo_queue** with the *ranked* optional paramter set to **true**
+            - **/lineup_create_captains** with the *ranked* optional paramter set to **true**
+            - **/match_manage**
+            - **/player_stats_downgrade**
+            - **/account_info**
+        `)
 
     const matchmakingEmbed = new EmbedBuilder()
         .setColor('#566573')
@@ -92,11 +100,13 @@ async function sendMessage() {
         .addFields([{ name: 'Want to report a bug or suggest a feature ?', value: 'Send a direct message to grass#6639' }])
 
     await channel!.send({ embeds: [titleEmbed, setupEmbed, matchmakingEmbed, otherEmbed] })
+    console.log("Message succesfully sent")
 }
 
-sendMessage().finally(async () => {
-    console.log("Message sent")
-    client.destroy()
-    process.exit()
-})
+sendMessage().
+    catch(console.error)
+    .finally(async () => {
+        client.destroy()
+        process.exit()
+    })
 
